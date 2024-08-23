c4091d4b32e5705106818f2934021c9a8f6fc638
이준서 <wnstj412@naver.com>
2024-08-23 10:07:15 +0900

    darkmode 사라진 거 추가

---
+++ DontGas/src/main/resources/static/js/darkmode.js
@@ -0,0 +1,17 @@
+let isDark = false;
+
+function toggleDarkMode() {
+    const image = document.getElementById('modbtn');
+    const existingLink = document.getElementById('theme-style');
+    const head = document.head;
+
+    if (isDark) {
+        existingLink.href = 'css/lightstyles.css';
+        image.src = image.getAttribute('data-dark-src');
+    } else {
+        existingLink.href = 'css/darkstyles.css';
+        image.src = image.getAttribute('data-light-src');
+    }
+
+    isDark = !isDark;
+}
