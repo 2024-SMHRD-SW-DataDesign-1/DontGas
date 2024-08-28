bdd638f14fcca719c30c2f38e21bdbfa51a1d3cb
이준서 <wnstj412@naver.com>
2024-08-28 16:49:27 +0900

    악취 레벨 정렬 적용 - 이준서

--- DontGas/src/main/resources/static/js/datatables.js
+++ DontGas/src/main/resources/static/js/datatables.js
@@ -1,47 +1,46 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-        new simpleDatatables.DataTable(datatablesSimple, {
+	const datatablesSimple = document.getElementById('datatablesSimple');
+	if (datatablesSimple) {
+		new simpleDatatables.DataTable(datatablesSimple, {
 			searchable: true,
 			labels: {
-           		perPage: "개 씩 보기",
-                info: "{start} - {end} / {rows}"
-            },
-            sortable: {
-            customSort: (a, b, column) => {
-				
-				if(column===0){
-					const orderA = a.querySelector('.sortingLabel').textContent;
-	                const orderB = b.querySelector('.sortingLabel').textContent;
-	                return orderA - orderB;
-				}
+				perPage: "개 씩 보기",
+				info: "{start} - {end} / {rows}"
+			},
+			sortable: {
+				customSort: (a, b, column) => {
 
-                // 기본 한국어 로케일을 사용하여 다른 열에 대해 정렬
-                return a.localeCompare(b, 'ko', { sensitivity: 'base' });
-            }
-        },
-            columns: [
-			    { select: [2], sortable: false }, // 정렬 비활성화
-			]
+					if (column === 0) { // 첫 번째 열에 대해 커스텀 정렬 적용
+		
+		            const orderA = parseInt(a.querySelector('td img.o-level').getAttribute('data-order'), 10);
+		            const orderB = parseInt(b.querySelector('td img.o-level').getAttribute('data-order'), 10);
+		
+		            return orderA - orderB;
+		        }
+
+					// 기본 한국어 로케일을 사용하여 다른 열에 대해 정렬
+					return a.localeCompare(b, 'ko', { sensitivity: 'base' });
+				}
+			}
 		});
-    }
-    
-    
-    const datatablesModal = document.getElementById('datatablesModal');
-    if (datatablesModal) {
-        new simpleDatatables.DataTable(datatablesModal, {
+	}
+
+
+	const datatablesModal = document.getElementById('datatablesModal');
+	if (datatablesModal) {
+		new simpleDatatables.DataTable(datatablesModal, {
 			perPage: Number.MAX_SAFE_INTEGER,  // 한 페이지에 표시할 항목 수
 			perPageSelect: "",
 			sortable: false,
 			searchable: false,
 			info: false,
-		    labels: {
-		        perPage: "",
-		        info: "",
-		        noRows : "데이터가 없습니다."
-		    }
+			labels: {
+				perPage: "",
+				info: "",
+				noRows: "데이터가 없습니다."
+			}
 		});
-    }
-    
+	}
+
 });
